f095187ac2e41ffe92f5bbf2c4095090ac04cb6a
edrikfsteiner <edrikfsteiner@gmail.com>
2024-09-09 17:17:05 -0300

    Create Main

--- exercicio07/src/test/java/org/example/exercicio07/Exercicio07ApplicationTests.java
+++ exercicio07/src/test/java/org/example/exercicio07/Exercicio07ApplicationTests.java
@@ -1,2 +1,13 @@
+package org.example.exercicio07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercicio07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
 
 }
