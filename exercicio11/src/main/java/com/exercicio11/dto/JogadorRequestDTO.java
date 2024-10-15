f67e0b549848e7e054cd87d37f4d84b608fc383b
edrikfsteiner <edrikfsteiner@gmail.com>
2024-10-15 20:20:15 -0300

    Create DTOs

---
+++ exercicio11/src/main/java/com/exercicio11/dto/JogadorRequestDTO.java
@@ -0,0 +1,3 @@
+package com.exercicio11.dto;
+
+public record JogadorRequestDTO(String nome, String apelido) {}
---
+++ exercicio11/src/main/java/com/exercicio11/dto/JogadorResponseDTO.java
@@ -0,0 +1,3 @@
+package com.exercicio11.dto;
+
+public record JogadorResponseDTO(String nome, String apelido, int habilidade, String statusMessage) {}
